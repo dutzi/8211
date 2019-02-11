51a4bb13729abfe16bdfbd1311fefec0cafb688c
Eldad Bercovici <eldad@spot.im>
2019-02-11 03:40:57 +0200

    added date picker

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
 import schema from '../secrets/schema';
+import _ from 'lodash';
 
 import Form from 'react-jsonschema-form';
 
@@ -8,18 +9,40 @@ class MyForm extends React.PureComponent {
   constructor() {
     super();
 
-    const formData = JSON.parse(localStorage.getItem('data') || '{}');
-    this.state = { formData };
+    const date = localStorage.getItem('date') || '';
+    this.state = { date };
+
+    this.handleSelect = this.handleSelect.bind(this);
   }
 
   componentDidMount() {
     setInterval(() => {
       // this.forceUpdate();
     }, 1000);
+    setTimeout(() => {
+      this.setState({
+        loaded: true,
+      });
+    }, 1000);
+    this.loadData();
+  }
+
+  loadData() {
+    const formData = JSON.parse(
+      localStorage.getItem(`data-${this.state.date}`) || '{}',
+    );
+    this.setState({ formData });
   }
 
   handleChange(res) {
-    localStorage.setItem('data', JSON.stringify(res.formData));
+    if (!this.state.loaded) {
+      return;
+    }
+
+    localStorage.setItem(
+      `data-${this.state.date}`,
+      JSON.stringify(res.formData),
+    );
     this.setState({
       formData: res.formData,
     });
@@ -40,26 +63,69 @@ class MyForm extends React.PureComponent {
       .split('-');
     let hours = new Date().getHours();
     const isEndless = fromTo[0] === 'משמרת';
-    fromTo[0] = Number(fromTo[0]);
-    fromTo[1] = Number(fromTo[1]);
+    let from = Number(fromTo[0]);
+    let to = Number(fromTo[1]);
     if (hours < 12) {
       hours += 24;
     }
-    if (fromTo[1] < fromTo[0]) {
-      fromTo[1] = fromTo[1] + 24;
+    if (from < 12) {
+      from += 24;
     }
-    console.log(hours, fromTo[0], fromTo[1]);
-    return isEndless || (fromTo[0] <= hours && fromTo[1] > hours);
+    if (to < 12) {
+      to += 24;
+    }
+    if (to < from) {
+      to = to + 24;
+    }
+    return isEndless || (from <= hours && to > hours);
+  }
+
+  handleSelect(e) {
+    localStorage.setItem('date', this.refs.select.value);
+    this.setState(
+      {
+        date: this.refs.select.value,
+      },
+      () => {
+        this.loadData();
+      },
+    );
   }
 
   render() {
-    const { formData } = this.state;
+    const { formData, date } = this.state;
 
     const positions = Object.keys(schema.properties);
+
+    console.log(formData);
 
     return (
       <React.Fragment>
         <div className="form">
+          <select
+            ref="select"
+            onChange={this.handleSelect}
+            className="select"
+            value={date}
+          >
+            <option>11/2 (ב)</option>
+            <option>12/2 (ג)</option>
+            <option>13/2 (ד)</option>
+            <option>14/2 (ה)</option>
+            <option>15/2 (ו)</option>
+            <option>16/2 (ש)</option>
+            <option>17/2 (א)</option>
+            <option>18/2 (ב)</option>
+            <option>19/2 (ג)</option>
+            <option>20/2 (ד)</option>
+            <option>21/2 (ה)</option>
+            <option>22/2 (ו)</option>
+            <option>23/2 (ש)</option>
+            <option>24/2 (א)</option>
+            <option>25/2 (ב)</option>
+            <option>26/2 (ג)</option>
+            <option>27/2 (ד)</option>
+          </select>
           {/* <button onClick={this.handleReset.bind(this)}>איפוס</button> */}
           <Form
             schema={schema}
@@ -105,7 +171,12 @@ class MyForm extends React.PureComponent {
                       >
                         {Object.keys(positionSchema2.properties).map(
                           soldier => (
-                            <tr>{formData[position][position3][soldier]}</tr>
+                            <tr>
+                              {_.get(
+                                formData,
+                                `${position}.${position3}.${soldier}`,
+                              )}
+                            </tr>
                           ),
                         )}
                       </td>
